9f535f9d8a4f0f0efdf3551fedc4132cae4226dd
tadam93 <gophres@gmail.com>
2022-02-01 13:13:36 -0700

    Create Dynamo table and GSI

--- infra/lib/DataStack.ts
+++ infra/lib/DataStack.ts
@@ -1,24 +1,22 @@
 import { Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import { ManagedPolicy, Role, User } from 'aws-cdk-lib/aws-iam';
-
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { AttributeType, ProjectionType, Table } from 'aws-cdk-lib/aws-dynamodb';
 
 export class DataStack extends Stack {
 	readonly USER_ARN = 'arn:aws:iam::709312312393:user/adamtrev-cli';
-
 	readonly DYNAMO_DB_FULL_ACCESS_POLICY = 'arn:aws:iam::aws:policy/AmazonDynamoDBFullAccess';
+
+	readonly TABLE_NAME = 'TestbedTable';
 
 	constructor(scope: Construct, id: string, props?: StackProps) {
 		super(scope, id, props);
 
-		// The code that defines your stack goes here
+		this.createRole();
+		this.createTable();
+	}
 
-		// example resource
-		// const queue = new sqs.Queue(this, 'InfraQueue', {
-		//   visibilityTimeout: cdk.Duration.seconds(300)
-		// });
-
+	createRole() {
 		const dynamoRole = new Role(
 			this,
 			'DynamoRole',
@@ -31,5 +29,28 @@ export class DataStack extends Stack {
 			'DynamoManagedPolicy',
 			this.DYNAMO_DB_FULL_ACCESS_POLICY
 		));
+	}
+
+	createTable() {
+		const table = new Table(
+			this,
+			this.TABLE_NAME,
+			{
+				partitionKey: {
+					name: 'pk',
+					type: AttributeType.STRING
+				},
+				sortKey: {
+					name: 'sk',
+					type: AttributeType.STRING
+				}
+		});
+
+		table.addGlobalSecondaryIndex({
+			indexName: 'gsi1',
+			partitionKey: {name: 'gsi1-pk', type: AttributeType.STRING},
+			sortKey: {name: 'gsi1-sk', type: AttributeType.STRING},
+			projectionType: ProjectionType.ALL
+		});
 	}
 }
